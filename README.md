2d6b92e16e7dfdbd528ef0b322225d66066c2cc1
Cybercube125 <esteemcube125@gmail.com>
2021-05-25 19:51:38 +0100

    Pizza Problems

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # 100-Days-of-Code
-Coding Collaboratively with friends for 100 days using JavaScript and Python.
+Coding Collaboratively with friends for 100 days using JavaScript. I'm still struggling with the basics.
--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-//My first js code!
-console.log("hello world");
+"use strict";
+//My first js  code!
+//The main function that is used by index.html
 function myFunction (){
     document.getElementById("demo").innerHTML = "Hello JavaScript";
+    /*function pizzaMaker(){
+
+    }
+let PizzaTime = confirm("Do you wish to order some pizza?");
+if (PizzaTime) {
+    alert("Great!")
+    pizzaMaker()
+    let CheeseType = prompt("What type of cheese do you want? [Mozzarella/Cheddar]")
+    switch(CheeseType) {
+        case "Mozzarella" :
+            confirm(`Are you sure you want ${CheeseType} cheese on your`)
+            break;
+    }
+} else {
+    let Reason = prompt("Then why are you here then?");
+    if (Reason == "I wanted to tell you I hate pizza!") {
+        alert("OH COME ON!");  
+    }
+}*/
 }
