1f93664c663e798298d86f15ce92cd14bd1575c9
Cybercube125 <esteemcube125@gmail.com>
2021-05-26 19:44:25 +0100

    Survey Denial

--- index.js
+++ index.js
@@ -5,18 +5,29 @@ function myFunction (){
     document.getElementById("demo").innerHTML = "Hello JavaScript";
 }
 
-function pizzaTime (){
-    let startQuiz = confirm("It's Pizza Time! Complete 3 challenges to earn a free pizza from your local PizzaHut!");
+
+function pizzaTime (){//Activated by "Pizza time?" button
+    let startQuiz = confirm("It's Pizza Time! Complete 3 challenges to earn a free pizza from your local PizzaHut!");//Asks the userwhether they want to start the Pizza Challenges
     if (startQuiz) {
         let Score = 0;
         let Modify = confirm("Great! The first challenge will be a quiz!");
         let Answer1 = prompt(`Question 1!
         What is ${Modify ? "96 + 4":"3125 x 482"}?`)
+        let Answer2;
         switch (Answer1,Modify) {
+            
             case 100,true:
-                alert("Correct!");
+                Modify2 = confirm("Correct! You have earned a point!");
                 Score = ++Score;
                 alert(Score);
+            case 100,false:
+                Modify2 = confirm("Right answer for the wrong question, but hey I'll give you half a point.");
+                Score = Score + 0.5
+            default:
+                Modify2 = confirm("Completely incorrect. You didn't earn a point!");
         }
+        
+    } else {
+        alert("Oh... Ok then :C");
     }
 }
